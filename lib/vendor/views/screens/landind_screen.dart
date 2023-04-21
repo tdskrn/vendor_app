ff6e0d32dca64f19eeb1f17565198a4ebd8d50fa
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-04-20 22:05:26 -0300

    vendor finish

--- lib/vendor/views/screens/landind_screen.dart
+++ lib/vendor/views/screens/landind_screen.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vendor_app/vendor/models/vendor_user_models.dart';
+import 'package:vendor_app/vendor/views/auth/vendor_register_screen.dart';
+import 'package:vendor_app/vendor/views/screens/main_vendor_screen.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
@@ -13,65 +15,73 @@ class LandingScreen extends StatelessWidget {
         FirebaseFirestore.instance.collection('vendors');
     return Scaffold(
       body: StreamBuilder<DocumentSnapshot>(
-        stream: _vendorsStream.doc(_auth.currentUser!.uid).snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loading');
-          }
-          VendorUserModel vendorUserModel = VendorUserModel.fromJson(
-              snapshot.data!.data()! as Map<String, dynamic>);
-          return Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(
-                  vendorUserModel.storeImage.toString(),
-                  width: 90,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                vendorUserModel.bussinessName.toString(),
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Your application has been sent to shop admin\n Admin will get back you soon',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextButton(
-                onPressed: () async {
-                  await _auth.signOut();
-                },
-                child: Text(
-                  'Signout',
-                ),
-              ),
-            ],
-          ));
-        },
-      ),
+          stream: _vendorsStream.doc(_auth.currentUser!.uid).snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading');
+            }
+
+            if (!snapshot.data!.exists) {
+              return VendorRegistrationScreen();
+            }
+            VendorUserModel vendorUserModel = VendorUserModel.fromJson(
+                snapshot.data!.data()! as Map<String, dynamic>);
+
+            if (vendorUserModel.approved == true) {
+              return MainVendorScreen();
+            } else {
+              return Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.network(
+                      vendorUserModel.storeImage.toString(),
+                      width: 90,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    vendorUserModel.bussinessName.toString(),
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Your application has been sent to shop admin\n Admin will get back you soon',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      await _auth.signOut();
+                    },
+                    child: Text(
+                      'Signout',
+                    ),
+                  ),
+                ],
+              ));
+            }
+          }),
     );
   }
 }
